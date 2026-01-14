e8fe3a3a0d076fefae8c27cdce008c41f316d66a
anhhtjse <anhtht@techasians.com>
2026-01-15 02:57:13 +0700

    Feat: Configure Actuator exposure and fix HiController CircuitBreaker annotation

--- ticket-system-controller/src/main/java/com/ticketsystem/controller/resource/HiController.java
+++ ticket-system-controller/src/main/java/com/ticketsystem/controller/resource/HiController.java
@@ -1,11 +1,13 @@
 package com.ticketsystem.controller.resource;
 
 import com.ticketsystem.application.service.event.EventAppService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/v1/haha")
@@ -15,6 +17,8 @@ public class HiController {
     @Autowired
     private EventAppService eventAppService;
 
+
+    RestTemplate restTemplate = new RestTemplate();
 
     @GetMapping("/hi")
     @RateLimiter(name = "ticketService", fallbackMethod = "fallbackSayHi")
@@ -31,4 +35,15 @@ public class HiController {
         return eventAppService.sayHi();
     }
 
+
+    @GetMapping("/circuit")
+    @CircuitBreaker(name = "circuitBreakerService", fallbackMethod = "fallbackCircuitBreaker")
+    public String circuitBreakerTest() {
+        //create a resttemplate call to fakestoreapi.com/products/1.
+        return restTemplate.getForObject("https://fakestoreapi.com/products/1", String.class);
+    }
+
+    public String fallbackCircuitBreaker(Throwable t) {
+        return "Circuit breaker fallback response";
+    }
 }
